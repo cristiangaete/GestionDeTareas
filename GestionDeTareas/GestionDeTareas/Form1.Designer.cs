afa6881db3d84b28bfa36ad8a4f215b3329f7028
Cristian-PC\Cristian <cristiangaetejordan@gmail.com>
2019-06-27 05:20:25 -0300

    Creacion del proyecto

---
+++ GestionDeTareas/GestionDeTareas/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace GestionDeTareas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lstTask = new System.Windows.Forms.ListBox();
+            this.txtTast = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(12, 92);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(128, 23);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Añadir nueva Tarea";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // lstTask
+            // 
+            this.lstTask.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstTask.FormattingEnabled = true;
+            this.lstTask.Location = new System.Drawing.Point(12, 219);
+            this.lstTask.Name = "lstTask";
+            this.lstTask.Size = new System.Drawing.Size(776, 212);
+            this.lstTask.TabIndex = 1;
+            this.lstTask.SelectedIndexChanged += new System.EventHandler(this.lstTask_SelectedIndexChanged);
+            // 
+            // txtTast
+            // 
+            this.txtTast.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtTast.Location = new System.Drawing.Point(53, 177);
+            this.txtTast.Name = "txtTast";
+            this.txtTast.Size = new System.Drawing.Size(625, 20);
+            this.txtTast.TabIndex = 2;
+            this.txtTast.TextChanged += new System.EventHandler(this.txtTast_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 177);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Tarea";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(281, 92);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "Guardar";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(391, 92);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 6;
+            this.btnDelete.Text = "Eliminar";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.BtnDelete_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(504, 92);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 7;
+            this.btnCancel.Text = "Cancelar";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTast);
+            this.Controls.Add(this.lstTask);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ListBox lstTask;
+        private System.Windows.Forms.TextBox txtTast;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
+
---
+++ GestionDeTareas/GestionDeTareas/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GestionDeTareas
+{
+    public partial class Form1 : Form
+    {
+        private bool _IsNewTask;
+        private bool _HasChanges;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Reset();
+        }
+
+        private void Reset()
+        {
+            this.lstTask.Enabled = false;
+            this.btnDelete.Enabled = false;
+            this.btnSave.Enabled = false;
+            this.btnCancel.Enabled = false;
+            this.txtTast.Enabled = false;
+            this.btnAdd.Enabled = true;
+            this.txtTast.Text = "";
+            //Activamos o descativamos el listbox dependiendo de la cantidad
+            this.lstTask.Enabled = this.lstTask.Items.Count > 0;
+            this.lstTask.SelectedIndex = -1;
+
+            _HasChanges = false;
+
+        }
+
+        private void AddNewTask()
+        {
+            if (_HasChanges)
+            {
+                if (MessageBox.Show("¿Guardar cambios?", "Guardar", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                   if (!this.SaveChanges())
+                    {
+                        return;
+                    }
+                }
+            }
+            this.btnCancel.Enabled = true;
+            this.btnSave.Enabled = true;
+            this.btnAdd.Enabled = false;
+            this.txtTast.Enabled = true;
+            this.txtTast.Text = "";
+            this.txtTast.Focus();
+            _IsNewTask = true;
+            
+        }
+
+        private void DeleteTask()
+        {
+            if (MessageBox.Show("¿Esta seguro que desea elminar?", "Confirmar eliminacion", MessageBoxButtons.YesNo)
+                == DialogResult.Yes)
+            {
+                if (lstTask.SelectedIndex >= 0 && lstTask.SelectedIndex < lstTask.Items.Count)
+                {
+                    this.lstTask.Items.RemoveAt(this.lstTask.SelectedIndex);
+                    this.Reset();
+                }
+            }
+            
+        }
+
+        private bool SaveChanges()
+        {
+            if (txtTast.Text.Length == 0)
+            {
+                MessageBox.Show("Debe escribir un nombre para la tarea", "Guardar", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            if (_IsNewTask)
+            {
+                this.lstTask.Items.Add(this.txtTast.Text);
+                this.Reset();
+            }
+            else
+            {
+                //Aqui edita el valor del item de lstTask
+                this.lstTask.Items[this.lstTask.SelectedIndex] = this.txtTast.Text;
+                MessageBox.Show("Guardado correctamente");
+            }
+
+            return true;
+            
+            
+        }
+
+        private void Cancel()
+        {
+            if (_HasChanges)
+            {
+                if (MessageBox.Show("¿Guardar cambios?", "Guardar", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    if (!this.SaveChanges())
+                    {
+                        return;
+                    }
+                }
+            }
+
+            this.Reset();
+        }
+
+       private void LoadSelectedTask()
+        {
+            if (lstTask.SelectedIndex >= 0 && lstTask.SelectedIndex <  lstTask.Items.Count)
+            {
+                //Recupera la posiciopn de la tarea que seleccionamos
+                this.txtTast.Text = lstTask.Items[lstTask.SelectedIndex].ToString();
+
+                this.btnSave.Enabled = true;
+                this.btnDelete.Enabled = true;
+                this.btnCancel.Enabled = true;
+                
+                this.txtTast.Enabled = true;
+
+                _IsNewTask = false;
+            }
+            
+        }
+
+
+
+
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            this.DeleteTask();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            this.AddNewTask();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            this.SaveChanges();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Cancel();
+        }
+
+        private void lstTask_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.LoadSelectedTask();
+        }
+
+        private void txtTast_TextChanged(object sender, EventArgs e)
+        {
+            _HasChanges = true;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+           DialogResult result =  MessageBox.Show("¿Guardar cambios?", "Guardar", MessageBoxButtons.YesNoCancel);
+
+            if (result == DialogResult.Yes)
+            {
+                if (!this.SaveChanges())
+                {
+                    e.Cancel = true;
+                    return;
+                }
+            }
+            else if (result == DialogResult.No)
+            {
+
+            }
+            else if (result == DialogResult.Cancel)
+            {
+                e.Cancel = true;
+            }
+               
+            
+        }
+    }
+}
